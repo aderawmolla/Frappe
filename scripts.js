3ae35dfa7aae584038bd0246b81dbf6e3b92bbc4
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-07-21 12:31:24 +0300

    Update scripts.js
    add new description

--- scripts.js
+++ scripts.js
@@ -68,3 +68,68 @@ function setDifferenceAgain(child) {
     var difference = child.monthly_fill - child.should_use;
     frappe.model.set_value(child.doctype, child.name, 'difference', difference);
 }
+
+////script 3
+
+
+frappe.ui.form.on('Equipment Daily Time Utilization Register', {
+    refresh: function(frm) {
+        frm.fields_dict['utilization_register_table'].grid.on_row_add = function(doc, cdt, cdn) {
+            var child = locals[cdt][cdn];
+            setWorkedHours(child);
+        };
+       frm.fields_dict['utilization_register_table'].grid.on_row_add = function(doc, cdt, cdn) {
+            var child = locals[cdt][cdn];
+            setDifferenceOfKm(child);
+        };
+        frm.fields_dict['utilization_register_table'].grid.on_row_refresh = function(doc, cdt, cdn) {
+            var child = locals[cdt][cdn];
+            setWorkedHours(child);
+        };
+        frm.fields_dict['utilization_register_table'].grid.on_row_refresh = function(doc, cdt, cdn) {
+            var child = locals[cdt][cdn];
+            setDifferenceOfKm(child);
+        };
+    }
+});
+
+frappe.ui.form.on('Utilization Register Table', {
+    first_half_start:function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        setWorkedHours(child);
+    },
+    first_half_end: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        setWorkedHours(child);
+    },
+   second_half_start: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        setWorkedHours(child);
+    },
+    second_half_end: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        setWorkedHours(child);
+    },
+   initial:function(frm,cdt,cdn){
+    var child = locals[cdt][cdn];
+   setDifferenceOfKm(child);
+
+   },
+   final:function(frm,cdt,cdn){
+   var child = locals[cdt][cdn];
+   setDifferenceOfKm(child);
+   }
+
+
+});
+
+function setWorkedHours(child) {
+    var first_half = child.first_half_end - child.first_half_start;
+    var  second_half=child.second_half_end-child.second_half_start;
+ var worked_hours=first_half+second_half;
+    frappe.model.set_value(child.doctype, child.name, 'worked_hrs', worked_hours);
+}
+function setDifferenceOfKm(child) {
+    var difference = child.final - child.initial;
+    frappe.model.set_value(child.doctype, child.name, 'diff', difference);
+}

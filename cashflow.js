0cd71c8c736dbc8ef54922747ece88cc7ca13007
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-08-20 16:05:53 +0300

    Update cashflow.js
    update cashflow

--- cashflow.js
+++ cashflow.js
@@ -1,1 +1,86 @@
 
+frappe.ui.form.on('Cashflow Schedule', {
+    start_date: fetchAndAssign,
+    project: fetchAndAssign,
+    end_date: fetchAndAssign,
+});
+
+function fetchAndAssign(frm) {
+    if (frm) {
+        frappe.call({
+            method: 'frappe.client.get_list',
+            args: {
+                doctype: 'Operational Plan',
+                filters: {
+                    'start_date': frm.doc.start_date ? ['>', frm.doc.start_date] : undefined,
+                    'end_date': frm.doc.end_date ? ['<=', frm.doc.end_date] : undefined,
+                    'project': frm.doc.project ? frm.doc.project : undefined,
+                    'name': ['!=', frm.docname],
+                },
+            },
+            callback: function (response) {
+                if (response.message && Array.isArray(response.message)) {
+                    var records = response.message;
+                    if (records.length === 0) {
+                        frm.clear_table('cashflow_table');
+                        refresh_field('cashflow_table');
+                    } else {
+                        fetchAndLogAllFields(records[0].name, frm);
+                    }
+                }
+            }
+        });
+    }
+}
+
+function fetchAndLogAllFields(utilizationRegisterName, frm) {
+    frm.clear_table('cashflow_table');
+
+    frappe.call({
+        method: 'frappe.client.get_list',
+        args: {
+            doctype: 'Monthly Plan',
+            filters: {
+                'operational_plan': utilizationRegisterName,
+            },
+        },
+        callback: function (response) {
+            if (response.message && Array.isArray(response.message)) {
+                var monthlyPlans = response.message;
+
+                for (var i = 0; i < monthlyPlans.length; i++) {
+                    (function (monthlyPlan) {
+                        frappe.call({
+                            method: 'frappe.client.get_value',
+                            args: {
+                                doctype: 'Monthly Plan',
+                                filters: {
+                                    'name': monthlyPlan.name,
+                                },
+                                fieldname: ['equipment_total_cost','man_power_total_cost', 'material_total_cost']
+                            },
+                            callback: function (response) {
+                                if (response.message) {
+                                    console.log(`Monthly Plan: ${monthlyPlan.name}`);
+                                    console.log(`equipment Total Cost: ${response.message.equipment_total_cost}`);
+                                    console.log(`Man Power Total Cost: ${response.message.man_power_total_cost}`);
+                                    console.log(`Material Total Cost: ${response.message.material_total_cost}`);
+                                    // Add your logic for adding child records here
+                                } else {
+                                    console.log(`Unable to fetch costs for Monthly Plan: ${monthlyPlan.name}`);
+                                }
+
+                                if (i === monthlyPlans.length - 1) {
+                                    // This is the last iteration, refresh the field
+                                    refresh_field('cashflow_table');
+                                }
+                            }
+                        });
+                    })(monthlyPlans[i]);
+                }
+            } else {
+                console.log("No records found in the Monthly Plan for the given Operational Plan.");
+            }
+        }
+    });
+}

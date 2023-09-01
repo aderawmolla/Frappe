0a2445a0551ab0c6075911bbe46d399f0fe77560
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-09-01 07:12:39 +0300

    Update add_filter.sql
    add filter

--- add_filter.sql
+++ add_filter.sql
@@ -1,1 +1,19 @@
 
+frappe.query_reports["Machinery Scheduling Report"] = {
+	"filters": [
+		{
+			fieldname:"from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.month_start(), -1),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_days(frappe.datetime.month_start(),-1),
+			reqd: 1
+		},
+	]
+}

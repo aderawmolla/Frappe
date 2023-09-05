287c5557710e595ef337f9fb0407fe6aff81923b
lijsamuael <101696125+lijsamuael@users.noreply.github.com>
2023-09-05 01:57:18 -0700

    Update add_filter.sql

--- add_filter.sql
+++ add_filter.sql
@@ -25,3 +25,17 @@ frappe.query_reports["Cashflow Report"] = {
 		},
 	]
 }
+
+
+//payroll filtering
+
+frappe.query_reports["Employees Payroll Report"] = {
+    "filters": [
+        {
+            "fieldname": "payroll_date",
+            "label": __("Payroll Date"),
+            "fieldtype": "Date",
+            "width": "80"
+        }
+    ]
+}

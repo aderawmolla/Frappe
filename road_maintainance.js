b5167431a71a05b93345f60846c066dbbb5c6375
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-10-29 02:28:30 +0300

    Update road_maintainance.js
    commit

--- road_maintainance.js
+++ road_maintainance.js
@@ -5,11 +5,11 @@
 
 frappe.ui.form.on("Traffic Count Result", {
     onload: function (frm, cdt, cdn) {
-         console.log("start function");
+        console.log("start function");
         calculateStaffs(frm, cdt, cdn, "day");
         calculateStaffs(frm, cdt, cdn, "night");
-        calculateStaffs(frm, cdt, cdn,"two");
-        calculateStaffs(frm,cdt,cdn,"seasonal");
+        calculateStaffs(frm, cdt, cdn, "two");
+        calculateStaffs(frm, cdt, cdn, "seasonal");
         frm.refresh_field("traffic_count_result_table");
         frm.refresh_field("night_time_count");
         frm.refresh_field("two_days_count");
@@ -112,8 +112,8 @@ frappe.ui.form.on("Night Time Count", {
     },
 });
 frappe.ui.form.on("Seasonal Count", {
- 
-   
+
+
 });
 function calculateRowTotal(frm, cdt, cdn) {
     var child = locals[cdt][cdn];
@@ -140,7 +140,7 @@ function calculateAvarages(frm, cdt, cdn) {
 }
 
 function calculateStaffs(frm, cdt, cdn, whichStaff) {
-   console.log("come on")
+    console.log("come on")
     var carTotal = 0;
     var LRoverTotal = 0;
     var SBusTotal = 0;
@@ -151,56 +151,63 @@ function calculateStaffs(frm, cdt, cdn, whichStaff) {
     var TTrailorTotal = 0;
     var TotalTotal = 0;
     var table;
+   var table_field;
     var len;
     if (whichStaff == "day") {
-        table = "traffic_count_result_table";
+       table_field="traffic_count_result_table";
+        table = frm.doc.traffic_count_result_table;
         len = 3;
     }
     else if (whichStaff == "night") {
-        table = "night_time_count";
+table_field="night_time_count";
+        table = frm.doc.night_time_count;
         len = 1;
 
     }
     else if (whichStaff == "two") {
-        table = "two_days_count";
+        table_field="two_days_count";
+        table = frm.doc.two_days_count;
         len = 1;
 
     }
     else {
-        table = "seasonal_count";
+   table_field="seasonal_count";
+        table = frm.doc.seasonal_count;
         i = 5;
     }
     console.log("does this excuted")
-     var tableDocs = frm.doc[table];
-     var length =
+    var tableDocs = frm.doc[table];
+    var length =
         tableDocs.length;
-    console.log("the length is",length)
-    if(whichStaff!="seasonal"){
+    console.log("the length is", length)
+    if (whichStaff != "seasonal") {
         $.each(tableDocs, function (i, d) {
-        carTotal += d.car;
-        LRoverTotal += d.lover;
-        SBusTotal += d.s_bus;
-        LBusTotal += d.l_bus;
-        STruckTotal = d.struck;
-        MTruckTotal = d.mtruck;
-        HTruckTotal = d.htruck;
-        TTrailorTotal = d.ttrailor;
-        TotalTotal = d.total;
-    });
+            carTotal += d.car;
+            LRoverTotal += d.lover;
+            SBusTotal += d.s_bus;
+            LBusTotal += d.l_bus;
+            STruckTotal = d.struck;
+            MTruckTotal = d.mtruck;
+            HTruckTotal = d.htruck;
+            TTrailorTotal = d.ttrailor;
+            TotalTotal = d.total;
+        });
 
-        
+
     }
-  
+
 
     for (i = 0; i <= len; i++) {
-        var child = frm.add_child(`${table}`);
-        frm.refresh_field(`${table}`);
-       
+        var child = frm.add_child(`${table_field}`);
+        frm.refresh_field("traffic_count_result_table");
+        frm.refresh_field("night_time_count");
+        frm.refresh_field("two_days_count");
+        frm.refresh_field("seasonal_count");
     }
     //total
 
 
-    if (whichStaff=="seasonal") {
+    if (whichStaff == "seasonal") {
         tableDocs[length].name = "seasonal 1 ADT (H)";
         tableDocs[length + 1].name = "seasonal 2 ADT(I)";
         tableDocs[length + 2].name = "seasonal 3 ADT(J)";
@@ -209,7 +216,7 @@ function calculateStaffs(frm, cdt, cdn, whichStaff) {
         tableDocs[length + 5].name = "AADT(K*L) OR L";
     }
     else {
- 
+
         tableDocs[length].date = "Total SUm OF Column A";
 
         tableDocs[length].car = carTotal;

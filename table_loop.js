27d19545f0e96585e0151bf03e70bb0aa81f8573
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-11-10 03:03:05 +0300

    Update table_loop.js

--- table_loop.js
+++ table_loop.js
@@ -1,1 +1,23 @@
-
+  frm.doc.technical_result = [];
+                var cards = response.message;
+                console.log("the response message", cards);
+                if (cards) {
+                    console.log("cards executed");
+                    $.each(cards, function (index, row) {
+                        var child = frappe.model.add_child(frm.doc, "technical_result");
+                        child.criteria = row.criteria;
+                        child.max_score = row.max_score;
+                    });
+                    refresh_field("technical_result");
+                    var maxScore = 0;
+                    try {
+                        $.each(frm.doc.technical_result, function (i, row) {
+                            maxScore += parseFloat(row.max_score);
+                            frm.set_value("max_percentage", maxScore);
+                            refresh_field("max_percentage");
+                        });
+                        console.log("max score obtained", maxScore);
+                    } catch (error) {
+                        console.log("error detected", error);
+                    }
+                }

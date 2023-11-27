1cb28ae73cb3d2b2c4cabcc7dadca0a49b2fbe20
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-11-27 21:52:14 +0300

    Update date_convertor.js

--- date_convertor.js
+++ date_convertor.js
@@ -1,1 +1,13 @@
 
+frappe.ui.form.on("Cannibalization request Form", {
+    schedule_date_ec: function(frm) {
+        if(frm.doc.schedule_date_ec){
+            var date = convertDateTOGC(frm.doc.schedule_date_ec.toString());
+            var dateObject = new Date(date);
+    // Format the date as a string in a desired format
+           var formattedDate = dateObject.toISOString().slice(0, 10);  // YYYY-MM-DD
+            frm.set_value("date", formattedDate);
+            frm.refresh_field("date")
+        }
+    },
+});

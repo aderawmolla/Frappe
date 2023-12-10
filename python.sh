9f6e167bf19241b4a5062c26f2948401a45a80ee
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2023-12-10 13:48:39 +0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,1 +1,43 @@
 
+   52  mkdir kviy
+   53  cd kviy
+   54  pip install kivy
+   55  sudo apt install python3-pip
+   56  pip install kivy
+   57  pip install kivymd
+   58  ls
+   59  py -m venv birhan
+   60  sudo apt install pythonpy
+   61  ls
+   62  py -m venv birhan
+   63  python3.8 -m venv birhan
+   64  py -m venv birhan
+   65  python -m venv birhan
+   66  python3 -m venv birhan
+   67  sudo     apt install python3.10-venv
+   68  python3 -m venv birhan
+   69  ls
+   70  source birhan/bin/activate
+   71  ls
+   72  cd birhan
+   73  ls
+   74  cd ..
+   75  cd birhan
+   76  pip install kivy
+   77  pip install kivymd
+   78  ls
+   79  pip install rembg
+   80  pip install pytube
+   81  pip install PyPDF2
+   82  pip install moviepy
+   83  pip install yt-dlp
+   84  pip install faker
+   85  pip install pdf2docx
+   86  pip install pandas
+   87  pip install numpy
+   88  pip install scikit-learn
+   89  pip install matplotlib
+   90  pip install nltk
+   91  pip install beautifulsoup4
+   92  pip install opencv-python
+   93  pip install tensorflow
